7f30644151730b1e26149f947a7cca996faef6cb
Long Nguyen <hoanglong1622000@gmail.com>
2021-06-03 13:01:41 +0700

    switching from db sessionmarker to db engine

--- scheduler.py
+++ scheduler.py
@@ -3,15 +3,20 @@ import time
 from datetime import datetime
 
 from crawl import initialize, get_data_to_db
-from dbconnector import get_db_session
+from dbconnector import create_db_engine
 
-def call_crawler(db):
+def call_crawler(engine):
     print("Crawler called at:", datetime.now())
+    # Create new session for database connector from the engine
+    db = engine.connect()
+    # Crawl data from RSS feeds and upload those to the database
     get_data_to_db(db)
+    # Close connection of the db connector
+    db.close()
 
-def scheduling(db):
+def scheduling(engine):
     # loop every 2 hours
-    schedule.every(2).hours.do(lambda: call_crawler(db))
+    schedule.every(2).hours.do(lambda: call_crawler(engine))
 
     while 1:
         schedule.run_pending()
@@ -19,5 +24,5 @@ def scheduling(db):
 
 if __name__ == '__main__':
     initialize()
-    db = get_db_session()
-    scheduling(db)
+    engine = create_db_engine()
+    scheduling(engine)
